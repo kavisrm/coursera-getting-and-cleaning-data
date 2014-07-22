b3c2c49dd88b94e09c1b85d46accc502dd65a1bf
kavisrm <kavi_ciet@yahoo.co.in>
2014-07-21 23:19:19 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,56 @@
-setwd("~/datascience/3. Obtaining Data/Project/")
-training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
-training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
-training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+root.dir <- "UCI HAR Dataset"
+data.set <- list()
 
-testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
-testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+message("loading features.txt")
+data.set$features <- read.table(paste(root.dir, "features.txt", sep="/"), col.names=c('id', 'name'), stringsAsFactors=FALSE)
 
-activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+message("loading activity_features.txt")
+data.set$activity_labels <- read.table(paste(root.dir, "activity_labels.txt", sep="/"), col.names=c('id', 'Activity'))
 
-# Read features and make the feature names better suited for R with some substitutions
-features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
-features[,2] = gsub('-mean', 'Mean', features[,2])
-features[,2] = gsub('-std', 'Std', features[,2])
-features[,2] = gsub('[-()]', '', features[,2])
 
-# Merge training and test sets together
-allData = rbind(training, testing)
+message("loading test set")
+data.set$test <- cbind(subject=read.table(paste(root.dir, "test", "subject_test.txt", sep="/"), col.names="Subject"),
+                       y=read.table(paste(root.dir, "test", "y_test.txt", sep="/"), col.names="Activity.ID"),
+                       x=read.table(paste(root.dir, "test", "x_test.txt", sep="/")))
 
-# Get only the data on mean and std. dev.
-colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
-# First reduce the features table to what we want
-features <- features[colsWeWant,]
-# Now add the last two columns (subject and activity)
-colsWeWant <- c(colsWeWant, 562, 563)
-# And remove the unwanted columns from allData
-allData <- allData[,colsWeWant]
-# Add the column names (features) to allData
-colnames(allData) <- c(features$V2, "Activity", "Subject")
-colnames(allData) <- tolower(colnames(allData))
+message("loading train set")
+data.set$train <- cbind(subject=read.table(paste(root.dir, "train", "subject_train.txt", sep="/"), col.names="Subject"),
+                        y=read.table(paste(root.dir, "train", "y_train.txt", sep="/"), col.names="Activity.ID"),
+                        x=read.table(paste(root.dir, "train", "X_train.txt", sep="/")))
 
-currentActivity = 1
-for (currentActivityLabel in activityLabels$V2) {
-  allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
-  currentActivity <- currentActivity + 1
+rename.features <- function(col) {
+  col <- gsub("tBody", "Time.Body", col)
+  col <- gsub("tGravity", "Time.Gravity", col)
+  
+  col <- gsub("fBody", "FFT.Body", col)
+  col <- gsub("fGravity", "FFT.Gravity", col)
+  
+  col <- gsub("\\-mean\\(\\)\\-", ".Mean.", col)
+  col <- gsub("\\-std\\(\\)\\-", ".Std.", col)
+  
+  col <- gsub("\\-mean\\(\\)", ".Mean", col)
+  col <- gsub("\\-std\\(\\)", ".Std", col)
+  
+  return(col)
 }
 
-allData$activity <- as.factor(allData$activity)
-allData$subject <- as.factor(allData$subject)
+## Extracts only the measurements on the mean and standard deviation for each measurement.
 
-tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
-# Remove the subject and activity column, since a mean of those has no use
-tidy[,90] = NULL
-tidy[,89] = NULL
-write.table(tidy, "tidy.txt", sep="\t")
+tidy <- rbind(data.set$test, data.set$train)[,c(1, 2, grep("mean\\(|std\\(", data.set$features$name) + 2)]
+
+## Uses descriptive activity names to name the activities in the data set
+
+names(tidy) <- c("Subject", "Activity.ID", rename.features(data.set$features$name[grep("mean\\(|std\\(", data.set$features$name)]))
+
+## Appropriately labels the data set with descriptive activity names.
+
+tidy <- merge(tidy, data.set$activity_labels, by.x="Activity.ID", by.y="id")
+tidy <- tidy[,!(names(tidy) %in% c("Activity.ID"))]
+
+## Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+tidy.mean <- ddply(melt(tidy, id.vars=c("Subject", "Activity")), .(Subject, Activity), summarise, MeanSamples=mean(value))
+
+write.csv(tidy.mean, file = "tidy.mean.txt",row.names = FALSE)
+write.csv(tidy, file = "tidy.txt",row.names = FALSE)
+
